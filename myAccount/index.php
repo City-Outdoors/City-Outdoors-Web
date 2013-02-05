e01c19a229aa90407394840eec6534695a940a3e
James <james@jarofgreen.co.uk>
2013-02-05 16:14:55 +0000

    Add copyright notices

--- myAccount/index.php
+++ myAccount/index.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author James Baster  <james@jarofgreen.co.uk>
+ * @copyright City of Edinburgh Council & James Baster
+ * @license Open Source under the 3-clause BSD License
+ * @url https://github.com/City-Outdoors/City-Outdoors-Web
+ */
 require '../includes/src/global.php';
 
 
