464275349dc43e9d2f7758a985d35edc993ea36b
James <james@jarofgreen.co.uk>
2013-08-30 13:14:14 +0100

    Event page

---
+++ event.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author James Baster  <james@jarofgreen.co.uk>
+ * @copyright City of Edinburgh Council & James Baster
+ * @license Open Source under the 3-clause BSD License
+ * @url https://github.com/City-Outdoors/City-Outdoors-Web
+ */
+require 'includes/src/global.php';
+
+
+$event = Event::loadByID($_GET['id']);
+if (!$event) {
+	 die('Not found!');
+}
+
+$eventSearch = new EventSearch();
+$eventSearch->setAfterNow();
+	
+$tpl = getSmarty();
+$tpl->assign('eventSearch',$eventSearch);
+$tpl->assign('event',$event);
+$tpl->display('event.htm');
+
+
+
+
+
+
+
